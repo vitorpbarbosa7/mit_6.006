60925fbc0e86e42d08b536fff5b224575a9bdb2f
vpbmac <vitorpbarbosa7@gmail.com>
2023-11-09 06:39:04 -0300

    working for one book in each

--- psets/ps2-template/book_test.py
+++ psets/ps2-template/book_test.py
@@ -1,9 +1,5 @@
-
-
-
 B1 = [(2,2,3)]
 B2 = [(1,0,4)]
-
 
 cont = 0
 
@@ -20,24 +16,39 @@ while i < len(B1) and j < len(B2):
 	start_j = B2[j][1]
 	
 	end_i = B1[i][2]
-	end_j = B1[j][2]
+	end_j = B2[j][2]
 	
 	if start_j < start_i:
 		start_time = start_j
 		# finalizar um para abrir outro
 		end = start_i
-		
 		rooms = rooms_j
-		i += 1
 	else:
 		start_time = start_i
 		end = start_j
 		rooms = rooms_i
 
-	if end_j > end_i:
-		i += 1
-		rooms +=
-	
 	new_element = (rooms, start_time, end)
-	print(new_element)
 	B.append(new_element)
+	print(B)
+
+	print(end_i)
+	print(end_j)
+	if end_i < end_j:
+		print('passou aqui')
+		start_time = start_i
+		end = end_i
+		rooms = rooms_i + rooms_j
+		i += 1
+	
+	elif end_i > end_j:
+		start_time = start_i
+		end = end_j
+		rooms = rooms_i + rooms_j
+		j += 1
+
+	new_element = (rooms, start_time, end)
+	B.append(new_element)
+	print(B)
+	breakpoint()
+
