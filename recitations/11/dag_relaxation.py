b1760c80ae602ba8640c22c112af1fc2b48fe02a
vpbmac <vitorpbarbosa7@gmail.com>
2023-12-24 13:36:48 -0300

    dag relaxation working

--- recitations/11/dag_relaxation.py
+++ recitations/11/dag_relaxation.py
@@ -17,9 +17,28 @@ def dag_relaxation(Adj, s):
         for v in Adj[u]:
 
             print(f'{u} : {Adj[u]}')
-            # try_to_relax(Adj, w, d, parent, u, v)
+            try_to_relax(Adj, w, d, parent, u, v, s)
     
     return d, parent
+
+def try_to_relax(Adj, w, d, parent, u, v, s):
+
+    old_d = d[v]
+    if d[v] > d[u] + w(Adj, u, v):
+        print(f'\nNew path found, because')
+        print(f'{d[v]} > {d[u]} + {w(Adj, u, v)}')
+        d[v] = d[u] + w(Adj, u, v)
+        print(f'distance from {s} to {v} updated from {old_d} to {d[v]}')
+
+        # update new parent pointer for v, which will come from u
+        # since we found a new path 
+        parent[v] = u
+
+def w(Adj, u, v):
+
+    return Adj[u][v]
+
+
 
 
 if __name__ == '__main__':
@@ -37,4 +56,7 @@ if __name__ == '__main__':
     
     s = 'e'
     
-    dag_relaxation(W, s)
+    d, parent = dag_relaxation(W, s)
+    
+    print(f'\nDistances from s to connected vertices:\n {d}')
+    print(f'\n Parent pointers:\n {parent}')
