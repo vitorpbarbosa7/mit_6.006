b822f3d64145141f1aa58fbb599d0d0ff96896dd
vpbmac <vitorpbarbosa7@gmail.com>
2024-01-23 08:16:25 -0300

    parent pointers finally created in a decent dict

--- recitations/16/contigous_largest_sum_subarray.py
+++ recitations/16/contigous_largest_sum_subarray.py
@@ -1,13 +1,17 @@
 
 A = [-9,1,-5,4,3,-6,7,8,-2]
 n = len(A)
-memo = [None for _ in range(n)]
+memo = [(None, None) for _ in range(n)]
+global_parent = [None, None]
 def x(i):
+    print('\nStack')
+    print(A[:i])
+    # breakpoint()
 
     #Base Case:
     #reached an empty array
     if i >= n:
-        return 0
+        return 0, []
     
     #Subproblem and relation
 
@@ -17,13 +21,34 @@ def x(i):
     # the trick here is that considering the A[i] as one of the options, breaks the continuity, starting a new subarray using it as the 
     # end of the array in the suffix
     # So we keep track of all the subsums
-    memo[i] = max(A[i], A[i] + x(i+1))
+    value, subsequence = x(i+1)
+
+    if A[i] + value > A[i]:
+        # coming back in the stack, if the next value going up is good, we add it together with the subsequence returned by 
+        # deeper levels in the stack 
+        subsequence = [A[i]] + subsequence
+    else:
+        # if not, we start a new subsequence with only that value
+        subsequence = [A[i]]
+
+    memo[i] = (max(A[i], A[i] + value), subsequence) 
+    print(memo)
     return memo[i]
 
 # Original Problem:
+print('\n\n Execute this shit')
 x(0)
+print('\n\nAnswer')
 print(memo)
-# largest_sum_subarray
-# print(largest_sum_subarray)
+dict_result = dict(memo)
+largest_subarray_sum = max(dict_result)
+largest_subarray_values = dict_result[largest_subarray_sum]
+print(largest_subarray_sum)
+print(largest_subarray_values)
+
+
+# possible to construct a graph of edges between them when A[i] + x(i+1) is considered
+# if not considered, no edge
+# then calculate the longest path 
 
 
--- recitations/16/mit_largest_sum_any_contigous_subarray.py
+++ recitations/16/mit_largest_sum_any_contigous_subarray.py
@@ -1,4 +1,5 @@
 
+A = [-9,1,-5,4,3,-6,7,8,-2]
 # bottom up implementation:
 def max_subarray_sum(A):
     # memo, keep track of each prefix solution
@@ -20,3 +21,15 @@ def max_subarray_sum(A):
 
 largest_sum_subarray = max_subarray_sum(A)
 print(largest_sum_subarray)
+
+
+# It is doing something like this:
+# when a dot replaces all previous underscores, means a new subarray will start, with only that element, because it was better
+# to keep only him and try others, then keep the sum of him with others
+# each row corresponds to a k we kept track of 
+# _
+# _ _ 
+# . . _
+# . . _ _
+# . . _ _ _ 
+# . . . . . _
