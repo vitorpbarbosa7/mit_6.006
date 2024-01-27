d2e7f458c8f76d1ee429c687df91aa0caf5d7aa2
vpbmac <vitorpbarbosa7@gmail.com>
2024-01-27 08:28:03 -0300

    longest increasing subsequence with memoization parent pointers for return the sequence

--- lectures/16/animation_lis.py
+++ lectures/16/animation_lis.py
@@ -1,8 +1,14 @@
 
+# Import Visualiser class from module visualiser
+from visualiser.visualiser import Visualiser as vs
+
 A = 'CARBOHYDRATE'
-A = 'EFAGBCD'
+# A = 'EFAGBCD'
 
 n = len(A)
+# Add decorator
+# Decorator accepts optional arguments: ignore_args , show_argument_name, show_return_value and node_properties_kwargs
+@vs(node_properties_kwargs={"shape":"record", "color":"#f57542", "style":"filled", "fillcolor":"grey"})
 def x(i):
     print('\nStack frame created')
     print(f'Suffix to use brute force with others: {A[i:]}')
@@ -13,6 +19,9 @@ def x(i):
         return 0
 
     lis_js = [0]*(n)
+    # from current i, explore all letters from it
+    # to register in the lis_js[j] how many letters exist bigger than in it 
+    # use the recursive call to get increasing behaviour
     for j in range(i, n, 1):
         print(f'Compare {A[i]} and {A[j]}')
         # breakpoint()
@@ -25,7 +34,7 @@ def x(i):
             # and for j, how many can I put on top of each, after it?
             # that will respect that the first i goes before, than j, than we found from j, what can go after this j, using the recursion itself
             # and at the end of this recursion call we return that it is 1 (current i) + maximum number of less than, which is marked in the lis_js
-            lis_js[j] = x(j)
+            lis_js[j] = x(i=j)
 
     # we enforce that i is in the answer, maybe the longest increasing subsequence will not have
     # any more element, but i will be there 
@@ -33,13 +42,16 @@ def x(i):
     # that is, for a single i, we will check which items are greater than j
     return 1 + max(lis_js)
 
-def original_problem():
+x(i=1)
+vs.make_animation("lis_CARBOHYDRATE.gif", delay=2)
 
-    all_lengths = []
-    for i in range(n):
-        all_lengths.append(x(i))
+# def original_problem():
 
-    return max(all_lengths)
+#     all_lengths = []
+#     for i in range(n):
+#         all_lengths.append(x(i))
 
-length = original_problem()
-print(length)
+#     return max(all_lengths)
+
+# length = original_problem()
+# print(length)
--- lectures/16/memo_lis.py
+++ lectures/16/memo_lis.py
@@ -1,8 +1,11 @@
 
+import numpy as np
 A = 'CARBOHYDRATE'
-A = 'EFAGBCD'
+# A = 'EFAGBCD'
 
 n = len(A)
+big_memo = [(0, [])]*len(A)
+memo = [[(0, [])]*len(A) for _ in range(len(A))]
 def x(i):
     print('\nStack frame created')
     print(f'Suffix to use brute force with others: {A[i:]}')
@@ -10,10 +13,16 @@ def x(i):
     # Base case
     # the index makes the suffix reaches its end
     if i == n:
-        return 0
+        base_case_count = 0
+        base_case_subsequence = []
+        return (base_case_count, base_case_subsequence)
 
-    lis_js = [0]*(n)
+    lis_js = [(0, [])]*n
     for j in range(i, n, 1):
+        if memo[i][j][0] != 0:
+            count, subsequence = memo[i][j]
+            lis_js[j] = (count, subsequence)
+
         print(f'Compare {A[i]} and {A[j]}')
         # breakpoint()
         # para ser considerado subsequence 
@@ -25,21 +34,39 @@ def x(i):
             # and for j, how many can I put on top of each, after it?
             # that will respect that the first i goes before, than j, than we found from j, what can go after this j, using the recursion itself
             # and at the end of this recursion call we return that it is 1 (current i) + maximum number of less than, which is marked in the lis_js
-            lis_js[j] = x(j)
+            jcount, jsub = x(j)
+            memo[i][j] = (jcount, jsub)
+            lis_js[j] = (jcount, jsub)
 
     # we enforce that i is in the answer, maybe the longest increasing subsequence will not have
     # any more element, but i will be there 
     # in the recursion call it will only increase if we find that A[i] < A[j]
     # that is, for a single i, we will check which items are greater than j
-    return 1 + max(lis_js)
+    count_sequence_dict = dict(lis_js)
+    largest_lis_without_i = max(count_sequence_dict)
+    largest_subsequence_without_i = count_sequence_dict[largest_lis_without_i]
+    count = 1 + largest_lis_without_i
+    subsequence = [A[i]] + largest_subsequence_without_i
+    big_memo[i] = (count, subsequence)
+    print(big_memo)
+    # breakpoint()
+    return  (count, subsequence)
 
 def original_problem():
 
-    all_lengths = []
+    all_lengths, all_subsequences = [], []
     for i in range(n):
-        all_lengths.append(x(i))
+        single_length, single_subsequence = x(i)
+        all_lengths.append(single_length)
+        all_subsequences.append(single_subsequence)
 
-    return max(all_lengths)
+    dict_result = {k: v for k,v in zip(all_lengths, all_subsequences)}
 
-length = original_problem()
-print(length)
+    max_length = max(dict_result)
+    max_subsequence = dict_result[max_length]
+
+    return max_length, max_subsequence
+
+final_length, final_subsequence = original_problem()
+print(final_length)
+print(final_subsequence)
