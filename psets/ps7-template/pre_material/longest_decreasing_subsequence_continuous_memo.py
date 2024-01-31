e0de63600b74c09a2be99455b61d5e3161d066a8
vpbmac <vitorpbarbosa7@gmail.com>
2024-01-31 07:00:07 -0300

    memoization added for longest decreasing continuous subsequence with singleton tuple numbers

--- psets/ps7-template/pre_material/longest_decreasing_subsequence_continuous_memo.py
+++ psets/ps7-template/pre_material/longest_decreasing_subsequence_continuous_memo.py
@@ -1,23 +1,23 @@
 
 A = [1,2,3,3,4,5,6,5,4,3,2,1,8,2,6,5,4,3]
-A = [19,17,10,5,4]
+# A = [19,17,10,5,4]
 
-memo = [None]*len(A)
-memo[-1] = 1
+memo = [(None, []) for _ in range(len(A))]
+# Base case for memo
+memo[-1] = (1, [A[-1]])
 def x(i):
     print('Stack frame called')
     print(f'Suffix: {A[i:]}')
+    # breakpoint()
 
     # single element at the end
     if i == len(A)-1:
-        return 1
+        return 1, [A[i]]
     
-    # if memo[i] is not None:
-    #     return memo[i] 
-
     # We want decreasing subsequence
     if A[i+1] < A[i]:
-        memo[i] = 1 + x(i+1)
+        suffix_sum, returned_subsequence = x(i+1)
+        memo[i] = (1 + suffix_sum, [A[i]] + returned_subsequence)
         return memo[i]
     
     else:
@@ -25,13 +25,16 @@ def x(i):
         # even if not returned here
         # and the memo array can be used in the next level
         x(i+1)
-        memo[i] = 1 
+        memo[i] = (1, [A[i]]) 
         return memo[i]
-    
+
 def original_problem(memo):
-    return max(memo)
+    memo = dict(memo)
+    max_length = max(memo)
+    subsequence = memo[max_length]
+    return max_length, subsequence
 x(0)
 
-max_length = original_problem(memo)
-print(memo)
-        
+max_length, subsequence = original_problem(memo)
+print(max_length)
+print(subsequence)
