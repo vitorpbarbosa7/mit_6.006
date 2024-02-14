a905f8f13a4f138e558e0a14fa8f4c5bc49c8ce3
vpbmac <vitorpbarbosa7@gmail.com>
2024-02-14 08:46:31 -0300

    ps 7-5 - (k-1)-(j mod k) prices till next day

--- psets/ps7-template/short_company.py
+++ psets/ps7-template/short_company.py
@@ -1,63 +1,50 @@
-from single_company_shortest_value import single_company_shortest_value
+# Only finally understood the solution, tried hard but could not solve
+
+# the thing was to realize considering always two consecutive days and forcing a item to be in the sequence
+# (like in the LIS), we get to form a longest decreasing subsequence, breaking when no item in a day is added 
 
 def short_company(C, P, n, k):
-    '''
-    Input:  C | Tuple of s = |C| strings representing names of companies
-            P | Tuple of s lists each of size nk representing prices
-            n | Number of days of price information
-            k | Number of prices in one day
-    Output: c | Name of a company with highest shorting value
-            S | List containing a longest subsequence of 
-              | decreasing prices from c that doesn't skip days
-    '''
-    c = C[0]
+    S = []
+    for i in range(len(C)):
+        p = P[i]
+        memo = [None for _ in range(n*k)]
+        
+        def dp(j):
+            if memo[j] is None:
+                #j = current item which will be included in the subsquence
+                # (k-1) - (j mod k) - items remaining in the same day
+                # k consider next day 
 
-    hashtable = {key: [value, 0, []] for key, value in zip(C, P)}
-    print(hashtable)
+                # this way we are considering always two consecutive days to have items 
+                # if they do not have items, the subsequencer will broke and start another one 
+                f = min(j + (k-1) - (j % k) + k, n*k - 1)
+                x, r = 1, None
 
-    for company_name, v in hashtable.items():
-          input = v[0]
-          print(input)
-          print(n)
-          print(k)
-        #   breakpoint()
-          max_length, subsequennce = single_company_shortest_value(input, n, k)
-          hashtable[company_name] = [input, max_length, subsequennce]
+                # considering from current item and items till next day
+                # here we return the number of items in the subsequence 
+                for d in range(j+1, f+1):
+                    x_, _ = dp(d)
 
-    print(hashtable)
-#     breakpoint()
+                    # price before must be higher than a later price
+                    if (p[j] > p[d]) and (1 + x_ > x):
+                        x, r = 1 + x_ , d
+                
+                memo[j] = (x, r)
 
-    c = max(hashtable, key=lambda company: hashtable[company][1])
-    S = hashtable[c][2]
+            return memo[j]
+        
+        best, opt = 0, 0
+        for j in range(n*k):
+            x, _ = dp(j)
+            if x > opt:
+                best, opt = j, x 
 
-    print('\n ---------- Final result:')
-    print(c)
-    print(S)
+        if opt > len(S):
+            c, S = C[i], []
 
+            while best != None:
+                S.append(p[best])
+                _, best = dp(best)
+        
+    S = tuple(S)
     return (c, S)
-
-
-if __name__ == '__main__':
-        # cpnk = (
-        #     ('SRTBOT', 'Try to Relax Inc.'),
-        #     (
-        #         (13, 25, 14, 2, 9, 17, 16, 13, 10, 16),
-        #         (12, 19, 7, 17, 5, 10, 5, 25, 4, 20),
-        #     ),
-        #     5,
-        #     2,
-        # )
-
-        cpnk = (
-            ('BitHub', 'GitBucket', 'GitBit'),
-            (
-                (33, 69, 91, 78, 19, 40, 13, 94, 10, 88, 43, 61, 72, 13, 46, 56, 41, 79, 82, 27, 71, 62, 57, 67, 34, 8, 71, 2, 12, 93),
-                (52, 91, 86, 81, 1, 79, 64, 43, 32, 94, 42, 91, 9, 25, 73, 29, 31, 19, 70, 58, 12, 11, 41, 66, 63, 14, 39, 71, 38, 91),
-                (16, 71, 43, 70, 27, 78, 71, 76, 37, 57, 12, 77, 50, 41, 74, 31, 38, 24, 25, 24, 5, 79, 85, 34, 61, 9, 12, 87, 97, 17),
-            ),
-            10,
-            3,
-        )
-        
-        short_company(*cpnk)
-        
